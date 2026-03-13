a24e14c5f98069e0b856207583b6a9415f399e74
戸高温人 <todakaharuto@tokouonninnoMacBook-Air.local>
2026-03-13 22:41:09 +0900

    ログイン画面の作成

---
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.application.service.UserApplicationService;
+
+
+@Controller
+@RequestMapping("/user")
+public class SignupController {
+	
+	@Autowired
+	private UserApplicationService userApplicationService;
+	
+	/*ユーザ登録画面を表示*/
+	@GetMapping("/signup")
+	public String getSignup(Model model, Locale locale) {
+		
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
+		model.addAttribute("genderMap", genderMap);
+		
+		return "user/signup";
+	}
+	
+	//ユーザ登録
+	@PostMapping("/signup")
+	public String postSignup() {
+		return "redirect:/login";
+	}
+}
