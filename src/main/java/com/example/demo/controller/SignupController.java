5f3c51b521da1e34d363d6c7f206ece576d74546
戸高温人 <todakaharuto@tokouonninnoMacBook-Air.local>
2026-03-31 13:42:24 +0900

    バリデーションメッセージの追加

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -6,15 +6,23 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.GroupOrder;
+import com.example.demo.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 	
 	@Autowired
@@ -22,7 +30,7 @@ public class SignupController {
 	
 	/*ユーザ登録画面を表示*/
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale) {
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
 		
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
@@ -32,7 +40,18 @@ public class SignupController {
 	
 	//ユーザ登録
 	@PostMapping("/signup")
-	public String postSignup() {
+	public String postSignup(Model model,Locale locale, @ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult) {
+		
+		//バリデーション
+		if(bindingResult.hasErrors()) {
+			//登録画面に戻る
+			return getSignup(model, locale, form);
+		}
+			
+		
+		log.info(form.toString());
+		
 		return "redirect:/login";
+		
 	}
 }
---
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -0,0 +1,34 @@
+package com.example.demo.form;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+	@NotBlank(groups = ValidGroup1.class)
+	@Email(groups = ValidGroup2.class)
+	private String userId;
+	
+	@NotBlank(groups = ValidGroup1.class)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
+	@Length(min = 8, max = 30, groups = ValidGroup2.class)
+	private String password;
+	
+	@NotBlank(groups = ValidGroup1.class)
+	private String UserName;
+	
+	@NotNull(groups = ValidGroup1.class)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date birthday;
+	@NotNull(groups = ValidGroup1.class)
+	private Integer gender;
+}
