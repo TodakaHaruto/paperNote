632ae15e57937f3a78fd310f9639358f2c6bc1d0
戸高温人 <todaharu0422@gmail.com>
2026-04-07 14:05:24 +0900

    認証ユーザ情報の受け渡しの実装

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,22 +1,35 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.user.model.MUser;
+import com.example.demo.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
 	//読んだ論文一覧
 	@GetMapping("/home")
-	public String getHome() {
+	public String getHome(@AuthenticationPrincipal org.springframework.security.core.userdetails.User loginUser, Model model) {
+		MUser user = userService.getLoginUser(loginUser.getUsername());
+		model.addAttribute("loginUser", user);
 		return "home/home";
 	}
 	
 	//読むべき論文一覧
 	@GetMapping("/toread")
-	public String getToread() {
-		//マイページを表示
+	public String getToread(@AuthenticationPrincipal org.springframework.security.core.userdetails.User loginUser, Model model) {
+		MUser user = userService.getLoginUser(loginUser.getUsername());
+		model.addAttribute("loginUser", user);
 		return "/home/toread";
 	}
 
